7215b28711329e62753a26fa0b11142b0c713509
Cedric Kirmayr <Cedric.kirmayr@gmail.com>
2024-05-25 13:42:18 -0400

    refactor: Update Jest configuration to use ts-jest for transforming TypeScript files

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -8,6 +8,9 @@ import type {Config} from 'jest';
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
 
   // Stop running tests after `n` failures
   // bail: 0,
--- backend/tests/app.test.ts
+++ backend/tests/app.test.ts
@@ -1,6 +1,17 @@
+import { envs } from '../src/adapters/envs';
+import { Server } from '../src/presentation/server';
 
-describe("Should test the app", () => {
-    test("Should return true" , () => {
-        expect(true).toBe(true)
+jest.mock('../src/presentation/server')
+
+describe("Testing App.ts", () => {
+    test("Should return true" , async() => {
+        await import('../src/app');
+        expect(Server).toHaveBeenCalledTimes(1);
+        expect(Server).toHaveBeenCalledWith({
+            port: envs.PORT,
+            routes: expect.any(Function),
+        });
+
+        expect(Server.prototype.start).toHaveBeenCalledWith();
     })
 })
