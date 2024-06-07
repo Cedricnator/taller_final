03313f65b9897c7ee40ae34b48f62821c6b127c3
cedricnator <c.kirmayr01@ufromail.cl>
2024-06-07 18:21:02 -0400

    feat(frontend): add themeProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Button } from './components/ui/button';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const handleCount = () => {
+    setCount(count + 1);
+  }
+
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='dark mx mx-auto bg-background h-screen text-primary'>
+      <div className=''>
+        <div className='py-4 px-2'>{count}</div>
+        <div>
+          <Button onClick={handleCount}>Click me</Button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
