9cde63e923028d717c1cc2d4f50037c041336e95
cedricnator <c.kirmayr01@ufromail.cl>
2024-06-08 03:28:47 -0400

    refactor: Update auth.service.ts to use bcrypt adapter for password hashing

--- backend/src/presentation/services/auth/auth.service.ts
+++ backend/src/presentation/services/auth/auth.service.ts
@@ -1,14 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 import { CustomError, RegisterUserDto } from "../../../domain";
 import { UserEntity } from "../../../domain/entities";
+import { BcryptAdapter } from '../../../adapters/bcryp.adapter';
 
 export class AuthService {
    private prisma = new PrismaClient();
+   private bcrypt = new BcryptAdapter();
+
    constructor(){
 
    }
 
    public async registerUser( registerUserDto: RegisterUserDto ) {
+      
       const existsUser = await this.prisma.user.findFirst({
          where: {
             email: registerUserDto.email
@@ -17,11 +21,13 @@ export class AuthService {
       if(existsUser) throw CustomError.badRequest('User already exists');
       
       try {
+         const hashedPassword = this.bcrypt.hash(registerUserDto.password);
+         
          const user = await this.prisma.user.create({
             data: {
                name:     registerUserDto.name,
                email:    registerUserDto.email,
-               password: registerUserDto.password
+               password: hashedPassword
             }
          });
 
