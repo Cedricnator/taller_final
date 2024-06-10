1d82b0c4285ac3a8cbcdb01aa7a51abac149f1b8
cedricnator <c.kirmayr01@ufromail.cl>
2024-06-10 03:02:55 -0400

    refactor: Update LogDataSource and LogEntity to adhere to coding conventions

--- logger/src/domain/datasource/log.datasource.ts
+++ logger/src/domain/datasource/log.datasource.ts
@@ -2,8 +2,8 @@ import { LogEntity, LogSeverityLevel } from '../entity/log.entity';
 
 // Implementamos la regla de negocio de nuestro dominio
 export abstract class LogDataSource {
-   abstract saveLog(log: LogEntity): Promise<void>;
-   abstract getLogsByOrigin(origin: string): Promise<LogEntity[]>;
+   abstract saveLog(log: LogEntity):                                 Promise<void>;
+   abstract getLogsByOrigin(origin: string):                         Promise<LogEntity[]>;
    abstract getLogsBySeverityLevel(severityLevel: LogSeverityLevel): Promise<LogEntity[]>;
-   abstract getLogs(): Promise<LogEntity[]>;
+   abstract getLogs():                                               Promise<LogEntity[]>;
 }
