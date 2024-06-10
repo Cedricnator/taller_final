e053cd7c7a8b104904f1b44e2651a4b4c07efd41
cedricnator <c.kirmayr01@ufromail.cl>
2024-06-10 03:22:57 -0400

    chore: Add CORS headers to backend and logger services

--- backend/src/presentation/server.ts
+++ backend/src/presentation/server.ts
@@ -30,6 +30,13 @@ export class Server {
             limits: { fileSize: 50 * 1024 * 1024 }
         }))
 
+        //* CORS
+        this.app.use((req, res, next) => {
+            res.append('Access-Control-Allow-Origin', ['*']);
+            res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+            next()
+        })
+
         //* Routes
         this.app.use(this.routes);
 
--- logger/src/presentation/server.ts
+++ logger/src/presentation/server.ts
@@ -28,6 +28,13 @@ export class Server {
         this.app.use(express.json());
         this.app.use(MorganAdapter.morganMiddleware())
 
+        //* CORS
+        this.app.use((req, res, next) => {
+            res.append('Access-Control-Allow-Origin', ['*']);
+            res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+            next()
+        })
+
         //* Routes
         this.app.use(this.routes);
 
