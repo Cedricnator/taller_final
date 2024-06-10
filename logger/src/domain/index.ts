c3988c14949221059a2a32f492a10ba95bff5d27
cedricnator <c.kirmayr01@ufromail.cl>
2024-06-10 00:59:29 -0400

    feat: add mongo datasource impl

---
+++ logger/src/domain/index.ts
@@ -0,0 +1,3 @@
+export * from './datasource/log.datasource';
+export * from './entity/log.entity';
+export * from './repository/log.repository';
---
+++ logger/src/infrastructure/datasource/mongo-log.datasource.ts
@@ -0,0 +1,20 @@
+import { LogModel } from "../../data";
+import { LogDataSource, LogEntity, LogSeverityLevel  } from "../../domain";
+
+export class MongoLogDataSource implements LogDataSource {
+    
+    async saveLog(log: LogEntity): Promise<void> {
+        const newLog = await LogModel.create(log);
+        //await newLog.save();
+        console.log('Mongo Log created: ', newLog.id );
+    }
+    
+    async getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+        const logs = await LogModel.find({
+            level: severityLevel
+        })
+
+        return logs.map( mongoLog => LogEntity.fromObject(mongoLog));
+    }
+
+}
