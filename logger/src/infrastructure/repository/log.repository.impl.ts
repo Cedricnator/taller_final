7de0fc8ed30895d51330e678c5b270e0ae590041
cedricnator <c.kirmayr01@ufromail.cl>
2024-06-10 01:01:11 -0400

    feat: add log.repository

---
+++ logger/src/infrastructure/repository/log.repository.impl.ts
@@ -0,0 +1,15 @@
+import { LogRepository, LogDataSource, LogEntity, LogSeverityLevel } from '../../domain';
+
+export class LogRepositoryImpl implements LogRepository {
+   constructor(
+      private readonly logDataSource: LogDataSource,
+   ) { }
+
+   async saveLog(log: LogEntity): Promise<void> {
+      return this.logDataSource.saveLog(log);
+   }
+
+   async getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+      return this.logDataSource.getLogs(severityLevel);
+   }
+}
