d4dfda237a9808f27386a3d44ab7d2e55d70acec
Cedric Kirmayr <Cedric.kirmayr@gmail.com>
2024-06-23 01:30:59 -0400

    feat: Add lazy loading for routes in Angular app

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,64 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Private Routes
+  {
+    path: 'dashboard',
+    title: 'Dashboard',
+    loadComponent: () => import('./dashboard/dashboard.component').then(c => c.DashboardComponent),
+    children: [
+      {
+        path: 'home',
+        title: 'Home',
+        loadComponent: () => import('./dashboard/pages/home/home.component').then(c => c.HomeComponent)
+      },
+      {
+        path: 'products',
+        title: 'Products',
+        loadComponent: () => import('./dashboard/pages/products/products.component').then(c => c.ProductsComponent)
+      },
+      {
+        path: 'product/:id',
+        title: 'Product',
+        loadComponent: () => import('./dashboard/pages/product/product.component').then(c => c.ProductComponent)
+      },
+      {
+        path: 'user',
+        title: 'User',
+        loadComponent: () => import('./dashboard/pages/user/user.component').then(c => c.UserComponent)
+      }
+    ]
+  },
+  // Public Routes
+  {
+    path: 'products',
+    loadComponent: () => import('./products/products.component').then(c => c.ProductsComponent),
+    children: [
+      {
+        path: 'home',
+        title: 'Home',
+        loadComponent: () => import('./products/pages/home/home.component').then(c => c.HomeComponent)
+      },
+      {
+        path: 'products',
+        title: 'Products',
+        loadComponent: () => import('./products/pages/products/products.component').then(c => c.ProductsComponent)
+      },
+      {
+        path: 'product',
+        title: 'Product',
+        loadComponent: () => import('./products/pages/product/product.component').then( c => c.ProductComponent)
+      },
+      {
+        path: 'about',
+        title: 'About',
+        loadComponent: () => import('./products/pages/about/about.component').then( c=> c.AboutComponent)
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  }
+];
---
+++ frontend/src/app/dashboard/pages/docs/docs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './docs.component.html',
+  styleUrl: './docs.component.scss'
+})
+export class DocsComponent {
+
+}
