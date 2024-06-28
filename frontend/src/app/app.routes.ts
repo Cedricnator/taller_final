d763e700784eebe819ec55f2f9b40aa898abe4d7
cedricnator <c.kirmayr01@ufromail.cl>
2024-06-28 16:59:26 -0400

    feat: Add lazy loading for dashboard and home components in app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {
+      path: 'dashboard',
+      title: 'Dashboard',
+      loadComponent: () => import('./dashboard/dashboard.component').then( c => c.DashboardComponent),
+      children: [
+         {
+            path: 'home',
+            title: 'Home',
+            loadComponent: () => import('./dashboard/pages/home/home.component').then( c => c.HomeComponent)
+         }
+      ]
+   }
+];
