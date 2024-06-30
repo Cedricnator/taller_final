378f7e9fa7a9ce1c384abfc0637a2956c32eb3ba
cedricnator <c.kirmayr01@ufromail.cl>
2024-06-30 14:02:17 -0400

    refactor: Update task component and task service

--- frontend/src/app/dashboard/pages/task/task.component.ts
+++ frontend/src/app/dashboard/pages/task/task.component.ts
@@ -11,4 +11,26 @@ import { TaskService } from '@dashboard/services/task.service';
 })
 export class TaskComponent {
   private _taskService = inject(TaskService)
+
+  public tasks = this._taskService.getTasks();
+  
+  public showTask(id: number){
+    return this._taskService.getTask(id);
+  }
+
+  public completeTask(id: number){
+    return this._taskService.completeTheTask(id);
+  }
+
+  public deleteTask(id: number){
+    return this._taskService.deleteTask(id);
+  }
+
+  public createTask(title: string, description: string, isDone: boolean){
+    return this._taskService.createTask(title, description, isDone);
+  }
+
+  public updateTask(title: string, description: string, isDone: boolean){
+    return this._taskService.updateTask(title, description, isDone);
+  }
 }
--- frontend/src/app/dashboard/services/task.service.ts
+++ frontend/src/app/dashboard/services/task.service.ts
@@ -16,10 +16,37 @@ interface TaskState {
 })
 export class TaskService {
 
-  constructor() { }
-
   #taskState = signal<TaskState>({
     task: []
   })
+
+  private _getTaskById(id: number){
+    return "Obteniendo tarea por id..."
+  }
+
+  public getTask(id: number){
+    return "Obteniendo tarea..."
+  }
+
+  public getTasks(){
+    return "Obteniendo todas las tareas..."
+  }
+
+  public createTask(title: string, description: string, isDone: boolean){
+    console.table({ title, description, isDone })
+    return "Creando tarea..."
+  }
+
+  public deleteTask(id: number){
+    return "Eliminando tarea..."
+  }
+
+  public updateTask(title: string, description: string, isDone: boolean){
+    return "Actualizando tarea..."
+  }
+
+  public completeTheTask(id: number){
+    return "Compleatando la tarea con el id: " + { id }
+  }
 
 }
