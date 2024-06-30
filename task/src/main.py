92a8084f801bfec4e31b0b8ec0b2096dfbcf4cfc
cedricnator <c.kirmayr01@ufromail.cl>
2024-06-30 00:58:31 -0400

    feat: Add CORS middleware to enable cross-origin resource sharing

--- task/src/main.py
+++ task/src/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from routes.app_route import app_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(app_routes, prefix="/api")
 
