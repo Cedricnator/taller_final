2979e9addbf8650841b8022f768c430f87d92d7d
cedricnator <c.kirmayr01@ufromail.cl>
2024-07-01 03:00:55 -0400

    refactor: Update sidebar and docs components to use Angular Material expansion panels

--- frontend/src/app/dashboard/pages/docs/docs.component.ts
+++ frontend/src/app/dashboard/pages/docs/docs.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-docs',
   standalone: true,
-  imports: [],
+  imports: [MatExpansionModule],
   templateUrl: './docs.component.html',
   styleUrl: './docs.component.css'
 })
 export class DocsComponent {
+  readonly panelOpenState = signal(false);
 
 }
