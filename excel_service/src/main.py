22b2747ce398adb2b936f00f0425e4cea9315e68
cedricnator <c.kirmayr01@ufromail.cl>
2024-07-01 01:03:57 -0400

    refactor: Remove unused code files

--- excel_service/src/main.py
+++ excel_service/src/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from routes.document_route import document
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 app.include_router(document, prefix="/document")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def hello_world():
